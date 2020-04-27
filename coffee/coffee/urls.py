8992f9f53f972ac158088637053c702bee944f1e
Alexis Segovia <alsedevelopment@gmail.com>
2020-04-26 17:46:18 -0700

    - project skeleton

--- coffee/coffee/urls.py
+++ coffee/coffee/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),
+    path('', include('storefront.urls'))
 ]
